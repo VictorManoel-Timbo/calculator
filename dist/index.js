cc61f255bc59749d898f6f85590b66d7f6a1b8df
gbrl-pnhr <143971816+gbrl-pnhr@users.noreply.github.com>
2024-06-14 14:35:50 -0300

    Revert "Operacao"

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,2 @@
 "use strict";
-var calculadora = {
-    soma: function (x, y) {
-        return x + y;
-    },
-    sub: function (x, y) {
-        return x - y;
-    },
-    mult: function (x, y) {
-        return x * y;
-    },
-    div: function (x, y) {
-        if (y == 0) {
-            return "Não existe divisão por 0";
-        }
-        else {
-            return x / y;
-        }
-    }
-};
+console.log("Hello. verificando se o código subiu.");
