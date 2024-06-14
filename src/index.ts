6a7653f00fc15943f45ac00a583db68f6514e1b4
gbrl-pnhr <143971816+gbrl-pnhr@users.noreply.github.com>
2024-06-14 16:32:02 -0300

    feature-function (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello. verificando se o código subiu.");
+interface Calculator{
+    add(x:number, y:number):number;
+    sub(x:number, y:number):number;
+    mult(x:number, y:number):number;
+    div(x:number, y:number):number|string;
+}
+
+let calculadora: Calculator = {
+    add(x:number, y:number){
+        return x + y;
+    },
+    sub(x:number, y:number){
+        return x - y;
+    },
+    mult(x:number, y:number){
+        return x * y;
+    },
+    div(x:number, y:number){
+        if(y == 0){
+            return "Não existe divisão por 0";
+        }else{ return x / y}
+    }
+};
