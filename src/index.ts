5171c3f1a9e73a36490dcfa30a4b371a0bb0a5b4
Victor Manoel Magalhães Timbó <68547537+VictorManoel-Timbo@users.noreply.github.com>
2024-06-14 16:59:35 -0300

    menu feature commit (#5)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+
+import prompt from 'prompt-sync'
+
+
 interface Calculator{
     add(x:number, y:number):number;
     sub(x:number, y:number):number;
@@ -5,7 +9,24 @@ interface Calculator{
     div(x:number, y:number):number|string;
 }
 
+
+function checkNum(num1:number, num2:number){
+    try {
+        if(!isNaN(num1) && !isNaN(num2)){
+            return true;
+        }else{
+            throw new TypeError();
+        }
+    } catch (e) {
+        console.log("Não foi digitado um número.");
+        return false;
+    }
+}
+
+let calculator: Calculator = {
+
 let calculadora: Calculator = {
+
     add(x:number, y:number){
         return x + y;
     },
@@ -20,4 +41,95 @@ let calculadora: Calculator = {
             return "Não existe divisão por 0";
         }else{ return x / y}
     }
+
 };
+
+let pt = prompt();
+let exit = 1;
+
+do{
+    let num1: number;
+    let num2: number;
+
+    console.log("----Calculadora----");
+    try {
+        let inputOp = pt("Digite a operação(1-soma,2-subtração,3-multiplicação,4-divisão, 0-Sair): ");
+        let op = Number(inputOp);
+        switch(op){
+            case 0:{
+                exit = 0;
+                break;
+            }
+            case 1:{
+                let inputNum = pt("Digite um numero: ");
+                num1 = Number(inputNum);
+
+                inputNum = pt("Digite outro numero: ");
+                num2 = Number(inputNum);
+                if(checkNum(num1, num2)){
+                    console.log("Resultado = ", calculator.add(num1,num2));
+                }else{
+                    console.log("Não foi possivel realizar o calculo.");
+                }
+                inputNum = pt("Deseja continuar?(1-sim, 0-não) ");
+                exit = Number(inputNum);
+                break;
+            }
+            case 2:{
+                let inputNum = pt("Digite um numero: ");
+                num1 = Number(inputNum);
+
+                inputNum = pt("Digite outro numero: ");
+                num2 = Number(inputNum);
+                if(checkNum(num1, num2)){
+                    console.log("Resultado = ", calculator.sub(num1,num2));
+                }else{
+                    console.log("Não foi possivel realizar o calculo.");
+                }
+                inputNum = pt("Deseja continuar?(1-sim, 0-não) ");
+                exit = Number(inputNum);
+                break;
+            }
+            case 3:{
+                let inputNum = pt("Digite um numero: ");
+                num1 = Number(inputNum);
+
+                inputNum = pt("Digite outro numero: ");
+                num2 = Number(inputNum);
+                if(checkNum(num1, num2)){
+                    console.log("Resultado = ", calculator.mult(num1,num2));
+                }else{
+                    console.log("Não foi possivel realizar o calculo.");
+                }
+                inputNum = pt("Deseja continuar?(1-sim, 0-não) ");
+                exit = Number(inputNum);
+                break;
+            }
+            case 4:{
+                let inputNum = pt("Digite um numero: ");
+                num1 = Number(inputNum);
+
+                inputNum = pt("Digite outro numero: ");
+                num2 = Number(inputNum);
+                if(checkNum(num1, num2)){
+                    console.log("Resultado = ", calculator.div(num1,num2));
+                }else{
+                    console.log("Não foi possivel realizar o calculo.");
+                }
+                inputNum = pt("Deseja continuar?(1-sim, 0-não) ");
+                exit = Number(inputNum);
+                break;
+            }
+            default:{
+                throw new TypeError();
+                break;
+            }
+        }
+    } catch (e) {
+        console.log("ERRO! Operador invalido.");
+    }
+
+}while(exit != 0);
+
+};
+
